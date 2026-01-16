77b0df57db08b230650f07944e901041c115f763
270505Nana <prajna270505@gmail.com>
2026-01-16 21:50:53 +0700

    Learning making a custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,24 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+//kita bisa ngebuat widget kita sendiri, semisal nih kita punya desain navbar. nah itu bisa kita pisah sendiri, biar nnti reusable
+// di beberapa screen, jdi kita tinggal panggil ajah
+
+//langkah-langkah
+
+// 1. buat class, nama bebas tapi harus kapital
+class GradientContainer extends StatelessWidget {
+@override
+// jadi kalau di flutter kita bisa extends atau inheri dari widget/function lain, disini pakai statelesswidget
+// kemudian karna kita extends stateless kita wajib membuat function namanya build dimana didalam build kita harus menambahkan widget
+  Widget build(context) {
+    return Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
@@ -27,8 +44,9 @@ void main() {
           child: const Center(
             child: Text("Hello nana", style: TextStyle(color: Colors.white, fontSize: 28)),
           ),
-        ),
-      ),
-    ),
-  );
+        );
+  } //build harus return widget
+
+// terus kalau di flutter/dart didepan nama fungsi itu menggambarkan return value type dari function kita
+
 }
