8af5985a2ef8d3c56c83da953fd3f65bdd546d61
270505Nana <prajna270505@gmail.com>
2026-01-23 17:53:56 +0700

    Learning add images

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -19,9 +19,15 @@ class GradientContainer extends StatelessWidget {
   // meskipun dia tipe variablenya final, tapi karena dia jenisnya list dia tetep bisa berubah meski final, makanya di kodingan bawahnya ngga bisa klo box decoration dipakaen const depannya
   // final List<Color>colors;
 
+  // construction 2, setting value
+  // nama class.nama var
+  // ini semisal kita mau setting langsung, jadi nnti ada dua pilihan bisa berdasarkan inputan di main, atau kalau di main ngga ada inputan bisa pakai yg default disini aja
+  const GradientContainer.purple({super.key})
+    : color1 = Colors.deepPurple,
+      color2 = Colors.indigo;
   //coba versi nggak dibikin list
-  final Color color1; 
-  final Color color2; 
+  final Color color1;
+  final Color color2;
   @override
   // jadi kalau di flutter kita bisa extends atau inheri dari widget/function lain, disini pakai statelesswidget
   // kemudian karna kita extends stateless kita wajib membuat function namanya build dimana didalam build kita harus menambahkan widget
@@ -39,7 +45,10 @@ class GradientContainer extends StatelessWidget {
       ),
 
       // sekarang udah bisa ubah textnya, sebagai parameter
-      child: Center(child: StyleText("Hello nana")),
+      // child: Center(child: StyleText("Hello nana")),
+      child: Center( 
+        child: Image.asset('assets/images/dice-2.png'),
+      ),
     );
   } //build harus return widget
 
--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-            Color.fromARGB(255, 33, 5, 109),
-            Color.fromARGB(255, 68, 21, 149)
-          ),
+        body: GradientContainer.purple(),
         ),
       ),
   );
